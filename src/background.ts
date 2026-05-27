a0b2a3286085904e93908999ba6004912f067331
alyrchikov <alyrchikov@yandex.ru>
2026-05-27 14:53:09 +0300

    Add welcome page

--- src/background.ts
+++ src/background.ts
@@ -1,3 +1,9 @@
-chrome.runtime.onInstalled.addListener(() => {
-  // no-op: background service worker kept for manifest entrypoint
+const WELCOME_PAGE_URL = "https://alexlyr.github.io/DXF-FIle-Viewer/welcome/";
+
+chrome.runtime.onInstalled.addListener((details) => {
+  if (details.reason !== chrome.runtime.OnInstalledReason.INSTALL) {
+    return;
+  }
+
+  chrome.tabs.create({ url: WELCOME_PAGE_URL });
 });

be9890f7d65858b154209242f9aaa575ef6e8478
alyrchikov <alyrchikov@yandex.ru>
2026-06-26 15:35:22 +0300

    page

--- src/background.ts
+++ src/background.ts
@@ -1,4 +1,7 @@
 const WELCOME_PAGE_URL = "https://alexlyr.github.io/dxf-file-viewer-pages/welcome/";
+const UNINSTALL_PAGE_URL = "https://alexlyr.github.io/dxf-file-viewer-pages/uninstall/";
+
+chrome.runtime.setUninstallURL(UNINSTALL_PAGE_URL);
 
 chrome.runtime.onInstalled.addListener((details) => {
   if (details.reason !== chrome.runtime.OnInstalledReason.INSTALL) {
